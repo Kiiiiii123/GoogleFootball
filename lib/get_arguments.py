2c6daf591eec619f740dcadf571b86b5662eda7a
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-07-07 15:25:43 +0800

    Update lib/get_arguments.py

--- lib/get_arguments.py
+++ lib/get_arguments.py
@@ -9,6 +9,8 @@ def get_args():
     parse.add_argument('--eps', type=float, default=1e-5, help='param for Adam optimizer')
     parse.add_argument('--save-dir', type=str, default='saved_models/', help='the folder to save models')
     parse.add_argument('--log-dir', type=str, default='logs/', help='the folder to save log')
+    parse.add_argument('--num-workers', type=int, help='the number of workers to collect samples')
+    parse.add_argument('--nsteps', type=int, default=128, help='the steps to collect samples')
     args = parse.parse_args()
 
     return args
