c00806023baccfdfcbd31508e0259376c02921dd
Kiiiiii123 <zhangchaoa@buaa.edu.cn>
2020-07-07 15:04:27 +0800

    Update lib/get_arguments.py

--- lib/get_arguments.py
+++ lib/get_arguments.py
@@ -3,7 +3,12 @@ import argparse
 
 def get_args():
     parse = argparse.ArgumentParser()
-    parse.add_argument('gamma', type=float, default=0.993, help='The discount factor of RL')
+    parse.add_argument('--gamma', type=float, default=0.993, help='The discount factor of RL')
+    parse.add_argument('--cuda', action='store_true', help='use cuda to do the training')
+    parse.add_argument('--env-name', type=str, default='academy_emtpy_goal_close', help='the environment name')
+    parse.add_argument('--eps', type=float, default=1e-5, help='param for Adam optimizer')
+    parse.add_argument('--save-dir', type=str, default='saved_models/', help='the folder to save models')
+    parse.add_argument('--log-dir', type=str, default='logs/', help='the folder to save log')
     args = parse.parse_args()
 
     return args
